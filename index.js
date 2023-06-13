2ab856b0e5c5e428cb71c65088fe5b8c87beac33
olegstan <olegstan@inbox.ru>
2023-06-13 19:34:38 +0300

    new logic with 2fa

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import { spawn } from 'child_process';
+import { spawn, fork } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
-let login, password, code;
+let login, password, group = 'CRM';
 
 process.argv.slice(2).forEach(function (val, index, array) {
   let value = val.toString();
@@ -14,34 +16,57 @@ process.argv.slice(2).forEach(function (val, index, array) {
       password = value;
       break;
     case 2:
-      code = value;
+      group = value;
       break;
   }
 });
 
-const openconnect = spawn('openconnect', [
-  'sslvpn.aton.ru',
-  '--background',
-  '--user=' + login,
-  '--authgroup=VPN_CRMUSER_2FA'
-]);
 
-openconnect.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
 
-openconnect.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+var __dirname = fs.realpathSync('.');
+var process = fork('node ' + __dirname + '/../2fa/src/index.js ' + login);
 
-  // Проверяем, запрашивается ли пароль
-  if (data.includes('Password:')) {
-    // Отправляем пароль вводом в stdin
-    openconnect.stdin.write(password + '\n');
-  }else if(data.includes('OTP')){
-    openconnect.stdin.write(code + '\n');
+var connect = function (code)
+{
+  const openconnect = spawn('openconnect', [
+    'sslvpn.aton.ru',
+    '--background',
+    '--user=' + login,
+    '--authgroup=' + group//VPN_CRMUSER_2FA or CRM
+  ]);
+
+  openconnect.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  openconnect.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+
+    // Проверяем, запрашивается ли пароль
+    if (data.includes('Password:')) {
+      // Отправляем пароль вводом в stdin
+      openconnect.stdin.write(password + '\n');
+    }else if(data.includes('OTP')){
+      openconnect.stdin.write(code + '\n');
+    }else if(data.includes('Connected as')){
+
+    }
+  });
+
+  openconnect.on('close', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
+}
+
+// listen for errors as they may prevent the exit event from firing
+process.on('message', function(message) {
+  console.log(message);
+
+  if(message.length === 6)
+  {
+    connect(message)
   }
 });
 
-openconnect.on('close', (code) => {
-  console.log(`Child process exited with code ${code}`);
-});
+
+
